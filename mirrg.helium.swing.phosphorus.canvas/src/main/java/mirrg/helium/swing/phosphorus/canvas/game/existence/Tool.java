bee0156fd568c9fa0b40a076ac308e5a47cd0a47
Mirrgie Riana <tacticsrealize2@gmail.com>
2016-11-17 03:30:32 +0900

    P.canvas.Tool: add: methods

--- mirrg.helium.swing.phosphorus.canvas/src/main/java/mirrg/helium/swing/phosphorus/canvas/game/existence/Tool.java
+++ mirrg.helium.swing.phosphorus.canvas/src/main/java/mirrg/helium/swing/phosphorus/canvas/game/existence/Tool.java
@@ -2,7 +2,9 @@ package mirrg.helium.swing.phosphorus.canvas.game.existence;
 
 import java.util.function.Consumer;
 
+import mirrg.helium.standard.hydrogen.event.EventManager;
 import mirrg.helium.swing.phosphorus.canvas.EventPhosphorusCanvas;
+import mirrg.helium.swing.phosphorus.canvas.game.EventPhosphorusGame;
 import mirrg.helium.swing.phosphorus.canvas.game.PhosphorusGame;
 
 public class Tool<G extends PhosphorusGame<?>> extends Existence<G>
@@ -13,15 +15,25 @@ public class Tool<G extends PhosphorusGame<?>> extends Existence<G>
 		super(game);
 	}
 
-	protected <T extends EventPhosphorusCanvas<?>> void registerEvent(Class<T> clazz, Consumer<T> listener)
+	protected <T> void registerEvent(EventManager<? super T> eventManager, Class<T> clazz, Consumer<T> listener)
 	{
-		game.canvas.event().registerRemovable(clazz, e -> {
+		eventManager.registerRemovable(clazz, e -> {
 			if (isDisposed) return false;
 
 			listener.accept(e);
 
 			return true;
 		});
+	}
+
+	protected <T extends EventPhosphorusCanvas<?>> void registerEvent(Class<T> clazz, Consumer<T> listener)
+	{
+		registerEvent(game.canvas.event(), clazz, listener);
+	}
+
+	protected <T extends EventPhosphorusGame> void registerGameEvent(Class<T> clazz, Consumer<T> listener)
+	{
+		registerEvent(game.event(), clazz, listener);
 	}
 
 }
