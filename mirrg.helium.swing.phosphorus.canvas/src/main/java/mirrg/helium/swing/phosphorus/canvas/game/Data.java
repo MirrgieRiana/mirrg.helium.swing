f939aed6e42424f18c1256a1c5396fd55beca2b8
Mirrgie Riana <tacticsrealize2@gmail.com>
2016-11-17 09:29:56 +0900

    fix: Data

--- mirrg.helium.swing.phosphorus.canvas/src/main/java/mirrg/helium/swing/phosphorus/canvas/game/Data.java
+++ mirrg.helium.swing.phosphorus.canvas/src/main/java/mirrg/helium/swing/phosphorus/canvas/game/Data.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 
 import mirrg.helium.swing.phosphorus.canvas.game.existence.DataEntity;
 import mirrg.helium.swing.phosphorus.canvas.game.view.DataView;
+import mirrg.helium.swing.phosphorus.canvas.game.view.DataViewBase;
 
 public class Data<G extends PhosphorusGame<?>>
 {
 
 	public ArrayList<DataEntity<? super G>> entities = new ArrayList<>();
-	public DataView view = new DataView();
+	public DataViewBase view = new DataView();
 
 	public void dispose(G game)
 	{
